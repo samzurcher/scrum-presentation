880c51e38ce8310e009a7746b796f2da2376121f
Sam Zurcher <sam@orelias.ch>
2012-11-10 09:43:25 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-scrum-presentation
+Scrum Presentation
 ==================
+
+A small presentation about Scrum. What have we learned during the last five years.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScrumPresentation::Application.config.secret_token = 'fcfd40e55d1e93ea749d5cd0185814cadba040226aa32c6c89b3c505cbd894c8df41093a6c3a37c2534d7cc188aa3330f26e5e06337e54f585208416b8563087'
