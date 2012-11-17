c8496467c0924065a74e5ae7a09c963c0408cc0e
Sam Zurcher <sam@orelias.ch>
2012-11-17 13:49:34 +0100

    delete precompiled stuff.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ ScrumPresentation::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( impress.js )
+  config.assets.precompile += %w( "#{Rails.root}/app/assets/javascripts/impress.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
